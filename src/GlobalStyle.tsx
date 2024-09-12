acbeaaace0c3952e90f5b6eb609d176122a8d012
kimtaejin3 <rlaxowls1316@naver.com>
2024-09-12 23:47:15 +0900

    feat: GlobalStyle 적용

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,29 @@
+import { Global, css } from "@emotion/react";
+
+const baseStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    list-style-type: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  button:focus {
+    outline: none;
+  }
+`;
+
+const GlobalStyle = () => <Global styles={baseStyle} />;
+
+export default GlobalStyle;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import "./index.css";
+import GlobalStyle from "./GlobalStyle.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
