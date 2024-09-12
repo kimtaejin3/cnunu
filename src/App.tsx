9f220a0971c55a9510bb718daac0f85ada756623
kimtaejin3 <rlaxowls1316@naver.com>
2024-09-12 21:50:48 +0900

    chore: eslint.config.js 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
+import reactLogo from "@/assets/react.svg";
 
 import viteLogo from "/vite.svg";
 
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [a, setA] = useState(0);
   return (
     <>
       <div>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,13 +2,13 @@
     스타일 관련 객체
 */
 export const COLORS = {
-  black: "#000",
-  white: "#fff",
-  grayscale50: "#FCFCFD",
-  grayscale800: "#384252",
-  primary150: "#B4F8F5",
-  primary450: "#B4F8F5",
-  primary600: "#00B4E6",
-  primary700: "linear-gradient(317deg, #00ADE3 1.81%, #0052C2 98.19%))",
-  primary750: "#1D5AAE",
+  black: '#000',
+  white: '#fff',
+  grayscale50: '#FCFCFD',
+  grayscale800: '#384252',
+  primary150: '#B4F8F5',
+  primary450: '#B4F8F5',
+  primary600: '#00B4E6',
+  primary700: 'linear-gradient(317deg, #00ADE3 1.81%, #0052C2 98.19%))',
+  primary750: '#1D5AAE'
 };
