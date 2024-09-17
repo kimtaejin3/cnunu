d48c35c7defb2d4efdd95626d22fe4a72a4d734c
kimtaejin3 <rlaxowls1316@naver.com>
2024-09-17 19:50:57 +0900

    feat: main 페이지 container, flex로 레이아웃 잡기

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,21 +6,43 @@ import Body from "@/styles/Body";
 const Main = () => {
   return (
     <Body sx={{ height: "100%" }}>
-      <header css={MainCss.header}>
-        <h1 css={MainCss.title}>CNU:NU</h1>
-        <div css={MainCss.subTitle}>충대 정보 싹 다 모았다</div>
-      </header>
-      <main css={MainCss.main}>...</main>
+      <div css={MainCss.container}>
+        <header css={MainCss.header}>
+          <h1 css={MainCss.title}>CNU:NU</h1>
+          <div css={MainCss.subTitle}>충대 정보 싹 다 모았다</div>
+        </header>
+        <div css={MainCss.contents}>
+          <main css={MainCss.main}>
+            <div>
+              <div css={MainCss.one}>1</div>
+              <div css={MainCss.two}>2</div>
+              <div css={MainCss.three}>3</div>
+              <div css={MainCss.four}>4</div>
+              <div css={MainCss.five}>5</div>
+              <div css={MainCss.six}>6</div>
+              <div css={MainCss.seven}>7</div>
+            </div>
+            <div></div>
+          </main>
+          <footer css={MainCss.footer}>
+            팀 소개 링크 / 제안: 1234@gmail.com
+          </footer>
+        </div>
+      </div>
     </Body>
   );
 };
 
 const MainCss = {
+  container: css`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  `,
   header: css`
+    flex-grow: 1;
     box-shadow: 0px 2px 4px 0px #00000026;
     background-color: ${COLORS.white};
-    padding: 1.8rem 0 0;
-    height: 11.4%;
     text-align: center;
   `,
   title: css`
@@ -32,8 +54,46 @@ const MainCss = {
     font-size: 1.4rem;
     margin-top: 0.5rem;
   `,
+  contents: css`
+    flex-grow: 30;
+  `,
   main: css`
-    height: 88.6%;
+    padding: 1.5rem;
+  `,
+  one: css`
+    grid-area: one;
+    background-color: dodgerblue;
+  `,
+  two: css`
+    grid-area: two;
+    background-color: dodgerblue;
+  `,
+  three: css`
+    grid-area: three;
+    background-color: dodgerblue;
+  `,
+  four: css`
+    grid-area: four;
+    background-color: dodgerblue;
+  `,
+  five: css`
+    grid-area: five;
+    background-color: dodgerblue;
+  `,
+  six: css`
+    grid-area: six;
+    background-color: dodgerblue;
+  `,
+  seven: css`
+    grid-area: seven;
+    background-color: dodgerblue;
+  `,
+
+  footer: css`
+    text-align: center;
+    margin-top: 2rem;
+    color: ${COLORS.grayscale800};
+    font-size: 1.4rem;
   `,
 };
 
