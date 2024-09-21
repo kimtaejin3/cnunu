d0cabcc6ccf2284fb8e5d2425a9c7e5167f69681
kimtaejin3 <rlaxowls1316@naver.com>
2024-09-21 16:41:56 +0900

    feat: MealTimeArea html 마크업

--- src/components/cafeteria/MealTimeArea.tsx
+++ src/components/cafeteria/MealTimeArea.tsx
@@ -12,9 +12,31 @@ const MealTimeArea = () => {
         <span>8월 21일 (목)</span>
         <button>{">"}</button>
       </div>
-      <div></div>
+      <ul
+        css={css`
+          display: flex;
+        `}
+      >
+        <li css={style.listItem}>
+          <button>아침</button>
+        </li>
+        <li css={style.listItem}>
+          <button>점심</button>
+        </li>
+        <li css={style.listItem}>
+          <button>저녁</button>
+        </li>
+      </ul>
     </div>
   );
+};
+
+const style = {
+  listItem: css`
+    flex-basis: 0;
+    flex-grow: 1;
+    text-align: center;
+  `,
 };
 
 export default MealTimeArea;
