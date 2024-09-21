32c75bcaacaa165f8a1cc02d43749f29bd18903a
kimtaejin3 <rlaxowls1316@naver.com>
2024-09-21 19:37:42 +0900

    feat: progressBar animation 추가

--- src/components/PercentBar.tsx
+++ src/components/PercentBar.tsx
@@ -1,4 +1,4 @@
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
 
 import { COLORS } from "@/constants";
 
@@ -6,6 +6,16 @@ type Props = {
   title: string;
   progress: number;
 };
+
+const animation = (progress: number) => keyframes`
+  0%{
+    width: 0%;
+  }
+
+  90% {
+    width: ${progress}%;
+  }
+`;
 
 const PercentBar = ({ title, progress }: Props) => {
   return (
@@ -18,27 +28,30 @@ const PercentBar = ({ title, progress }: Props) => {
       >
         {title}
       </div>
-      <div
-        css={css`
-          background: #e0e4ea;
-          height: 0.7rem;
-          border-radius: 0.2rem;
-          margin-top: 0.5rem;
-        `}
-      >
-        <div
-          css={css`
-            height: 0.7rem;
-            border-radius: 0.2rem;
-            background: ${COLORS.primary700};
-            box-shadow: 1px 1.5px 2.5px 1px #ffffff66 inset;
-            box-shadow: -1px -1.5px 4px 1px #a1ecfb66 inset;
-            width: ${progress}%;
-          `}
-        ></div>
+      <div css={style(progress).outer}>
+        <div css={style(progress).inner}></div>
       </div>
     </>
   );
 };
+
+const style = (progress: number) => ({
+  outer: css`
+    background: #e0e4ea;
+    height: 0.7rem;
+    border-radius: 0.2rem;
+    margin-top: 0.5rem;
+  `,
+
+  inner: css`
+    height: 0.7rem;
+    border-radius: 0.2rem;
+    background: ${COLORS.primary700};
+    box-shadow: 1px 1.5px 2.5px 1px #ffffff66 inset;
+    box-shadow: -1px -1.5px 4px 1px #a1ecfb66 inset;
+    animation: ${animation(progress)} 1s;
+    width: ${progress}%;
+  `,
+});
 
 export default PercentBar;
