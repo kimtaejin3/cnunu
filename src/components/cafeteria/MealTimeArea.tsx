a56edca45c388778adc8b8df904e0960fba0176c
kimtaejin3 <rlaxowls1316@naver.com>
2024-09-21 16:59:17 +0900

    feat: 학식시간 선택시 boreder 색 다르게

--- src/components/cafeteria/MealTimeArea.tsx
+++ src/components/cafeteria/MealTimeArea.tsx
@@ -1,5 +1,7 @@
 import { css } from "@emotion/react";
 
+import { COLORS } from "@/constants";
+
 const MealTimeArea = () => {
   return (
     <div
@@ -17,13 +19,13 @@ const MealTimeArea = () => {
           display: flex;
         `}
       >
-        <li css={style.listItem}>
+        <li css={style(false).listItem}>
           <button>아침</button>
         </li>
-        <li css={style.listItem}>
+        <li css={style(true).listItem}>
           <button>점심</button>
         </li>
-        <li css={style.listItem}>
+        <li css={style(false).listItem}>
           <button>저녁</button>
         </li>
       </ul>
@@ -31,12 +33,23 @@ const MealTimeArea = () => {
   );
 };
 
-const style = {
+const style = (isActive: boolean) => ({
   listItem: css`
     flex-basis: 0;
     flex-grow: 1;
     text-align: center;
+    border-bottom: ${isActive
+      ? `2px solid ${COLORS.primary800}`
+      : `2px solid ${COLORS.grayscale300}`};
+    padding-bottom: 1rem;
+    button {
+      font-family: SUIT;
+      color: ${isActive ? COLORS.black : "rgba(0, 0, 0, 0.5)"};
+      font-size: 16px;
+      font-weight: 700;
+      text-align: center;
+    }
   `,
-};
+});
 
 export default MealTimeArea;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,6 +7,7 @@ export const COLORS = {
   bg: "#e1e7eb",
   grayscale50: "#FCFCFD",
   grayscale100: "#f1f3f6",
+  grayscale300: "#C8CED9",
   grayscale800: "#384252",
   grayscale900: "#191D24",
   primary150: "#B4F8F5",
@@ -15,4 +16,5 @@ export const COLORS = {
   primary600: "#00B4E6",
   primary700: "linear-gradient(317.24deg, #00ADE3 1.81%, #0052C2 98.19%)",
   primary750: "#1D5AAE",
+  primary800: "#004098",
 };
