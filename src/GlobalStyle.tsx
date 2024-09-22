04e1ebb8fa72efb52a6c00c3cbdff85d71f8139f
kimtaejin3 <rlaxowls1316@naver.com>
2024-09-22 20:02:39 +0900

    design: MealTimeArea 컴포넌트 UI 스타일링

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -41,11 +41,23 @@ const baseStyle = css`
     outline: none;
   }
 
+  img {
+    vertical-align: top;
+  }
+
   @font-face {
     font-family: "SUIT-Regular";
     src: url("https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2")
       format("woff2");
-    font-weight: normal;
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "SUIT-SemiBold";
+    src: url("https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-SemiBold.woff2")
+      format("woff2");
+    font-weight: 600;
     font-style: normal;
   }
 
--- src/components/cafeteria/MealTimeArea.tsx
+++ src/components/cafeteria/MealTimeArea.tsx
@@ -1,5 +1,10 @@
 import { css } from "@emotion/react";
 
+import { CSSProperties } from "react";
+
+import LEFT_ARROW from "@/assets/icons/left_arrow.svg";
+
+import RIGHT_ARROW__ACTIVE from "@/assets/icons/right_arrow-active.svg";
 import { COLORS } from "@/constants";
 
 const MealTimeArea = () => {
@@ -9,23 +14,41 @@ const MealTimeArea = () => {
         margin-top: 0;
       `}
     >
-      <div>
-        <button>{"<"}</button>
-        <span>8월 21일 (목)</span>
-        <button>{">"}</button>
+      <div css={style.datepickArea}>
+        <button>
+          <img src={LEFT_ARROW} alt="left_arow__active" />
+        </button>
+        <div
+          css={css`
+            position: relative;
+          `}
+        >
+          <TodayTag
+            sx={{
+              position: "absolute",
+              top: "-1.6rem",
+              left: "50%",
+              transform: "translateX(-50%)",
+            }}
+          />
+          <h2>8.21 (목)</h2>
+        </div>
+        <button>
+          <img src={RIGHT_ARROW__ACTIVE} alt="right_arrow" />
+        </button>
       </div>
       <ul
         css={css`
           display: flex;
         `}
       >
-        <li css={style(false).listItem}>
+        <li css={listItemStyle(false)}>
           <button>아침</button>
         </li>
-        <li css={style(true).listItem}>
+        <li css={listItemStyle(true)}>
           <button>점심</button>
         </li>
-        <li css={style(false).listItem}>
+        <li css={listItemStyle(false)}>
           <button>저녁</button>
         </li>
       </ul>
@@ -33,23 +56,56 @@ const MealTimeArea = () => {
   );
 };
 
-const style = (isActive: boolean) => ({
-  listItem: css`
-    flex-basis: 0;
-    flex-grow: 1;
+const TodayTag = ({ sx }: { sx?: CSSProperties }) => {
+  return (
+    <span
+      style={sx}
+      css={css`
+        border-radius: 6.5rem;
+        background-color: ${COLORS.primary300};
+        padding: 0.1rem 0.8rem 0.2rem 0.8rem;
+      `}
+    >
+      today
+    </span>
+  );
+};
+
+const listItemStyle = (isActive: boolean) => css`
+  flex-basis: 0;
+  flex-grow: 1;
+  text-align: center;
+  border-bottom: ${isActive
+    ? `2px solid ${COLORS.primary800}`
+    : `2px solid ${COLORS.grayscale300}`};
+  padding-bottom: 1rem;
+  button {
+    font-family: SUIT;
+    color: ${isActive ? COLORS.black : "rgba(0, 0, 0, 0.5)"};
+    font-size: 16px;
+    font-weight: 700;
     text-align: center;
-    border-bottom: ${isActive
-      ? `2px solid ${COLORS.primary800}`
-      : `2px solid ${COLORS.grayscale300}`};
-    padding-bottom: 1rem;
-    button {
-      font-family: SUIT;
-      color: ${isActive ? COLORS.black : "rgba(0, 0, 0, 0.5)"};
-      font-size: 16px;
+  }
+`;
+
+const style = {
+  datepickArea: css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${COLORS.white};
+    padding: 2rem 0;
+    gap: 8rem;
+
+    h2 {
+      font-family: SUIT-SemiBold;
+      font-size: 2rem;
       font-weight: 700;
-      text-align: center;
+      line-height: 2.8rem;
+      letter-spacing: -0.02em;
+      text-align: left;
     }
   `,
-});
+};
 
 export default MealTimeArea;
