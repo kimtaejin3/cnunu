18addb31ba2e80028e65544aeb82eb9fb244294c
kimtaejin3 <rlaxowls1316@naver.com>
2024-09-29 20:11:31 +0900

    feat: 학과소식 페이지 Navigation bar 추가

--- src/pages/DepartmentNotification.tsx
+++ src/pages/DepartmentNotification.tsx
@@ -1,5 +1,12 @@
+import NavigationBar from "@/components/common/NavigationBar";
+import Notification from "@/components/common/Notification";
+
 const DepartmentNotification = () => {
-  return <div>..</div>;
+  return (
+    <div>
+      <NavigationBar title="학과소식" element={<Notification />} />
+    </div>
+  );
 };
 
 export default DepartmentNotification;
