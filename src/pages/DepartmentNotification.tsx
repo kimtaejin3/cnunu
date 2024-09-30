7e6fd35cdcf4ae63a527dc8b942a782fe489dab1
kimtaejin3 <rlaxowls1316@naver.com>
2024-09-30 09:31:04 +0900

    feat: 학과소식 페이지 NotificatoinCard 컴포넌트 추가

--- src/pages/DepartmentNotification.tsx
+++ src/pages/DepartmentNotification.tsx
@@ -1,12 +1,27 @@
+import { css } from "@emotion/react";
+
 import NavigationBar from "@/components/common/NavigationBar";
 import Notification from "@/components/common/Notification";
+import NotificationCard from "@/components/common/NotificationCard";
 import Search from "@/components/common/Search";
 
 const DepartmentNotification = () => {
   return (
     <div>
       <NavigationBar title="학과소식" element={<Notification />} />
-      <Search />
+      <div
+        css={css`
+          padding: 0 1.6rem;
+        `}
+      >
+        <Search />
+        <div>
+          <NotificationCard
+            category="공지사항"
+            title="학과 사무실 에어컨 보수공사로 인한 건물어쩌구"
+          />
+        </div>
+      </div>
     </div>
   );
 };
